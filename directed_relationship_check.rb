4547838a1ce4779051c48f1082de2b563a5d1e35
Moritz Schepp <moritz.schepp@gmail.com>
2019-12-17 16:57:08 -0800

    fixes errors

--- directed_relationship_check.rb
+++ directed_relationship_check.rb
@@ -8,21 +8,33 @@ puts "SIMULATION MODE" if SIMULATION
 require 'pry'
 require "#{KOR_ROOT}/config/environment"
 
-def validate(relationship, normal, reverse)
-  if [relationship, normal, reverse].any?{|e| e.nil?}
-    ids = [relationship.try(:id), normal.try(:id), reverse.try(:id)]
+def validate(relationship, normal, reversal)
+  if [relationship, normal, reversal].any?{|e| e.nil?}
+    ids = [relationship.try(:id), normal.try(:id), reversal.try(:id)]
     puts "incomplete data for validation: #{ids.inspect}"
   end
 
   if relationship.name != normal.relation_name ||
-     relationship.reverse_name != reverse.relation_name
+     relationship.reverse_name != reversal.relation_name
      relationship.id != normal.relationship_id ||
-     relationship.id != reverse.relationship_id
+     relationship.id != reversal.relationship_id
      
      binding.pry
      unless SIMULATION
        relationship.ensure_directed
        relationship.save!
      end
+  end
+end
+
+Relationship.find_each do |r|
+  validate(r, r.normal, r.reversal)
+end
+
+DirectedRelationship.find_each do |dr|
+  if dr.is_reversal?
+    validate(dr.relationship, dr.relationship.normal, dr)
+  else
+    validate(dr.relationship, dr, dr.relationship.reversal)
   end
 end
