65ca135baa83b3b6ac7366dee765e3b390cf79ce
Moritz Schepp <moritz.schepp@gmail.com>
2020-03-12 16:11:49 -0700

    added checks to webdav scripts

--- webdav_import.sh
+++ webdav_import.sh
@@ -9,7 +9,7 @@ CURRENT="$WEBDAV/archive/$TS"
 
 # make sure no previous run is active
 if ! -f $WEBDAV/done.txt ; then
-  echo 'a previous run has not finished yet'
+  echo 'a previous run has not finished yet' >> $CURRENT/log.txt
   exit 1
 fi
 rm $WEBDAV/done.txt
@@ -21,6 +21,16 @@ mkdir -p $WEBDAV/new
 sudo chown apache. $WEBDAV/new
 sudo chown kor. $CURRENT
 
+# some checks
+if ! -f $CURRENT/data.csv ; then
+  echo "$CURRENT/data.csv couldn't be found" >> $CURRENT/log.txt
+  exit 1
+fi
+if ! -d $CURRENT/images ; then
+  echo "$CURRENT/images couldn't be found" >> $CURRENT/log.txt
+  exit 1
+fi
+
 # set parameters for import script
 export KOR_ROOT="/home/kor/kor"
 export SIMULATION="true"
@@ -29,7 +39,7 @@ export IMAGES_DIR="$CURRENT/images"
 export CSV_FILE="$CURRENT/data.csv"
 
 # make a backup
-/home/kor/scripts.git/snapshot.sh &> $CURRENT/log.txt
+/home/kor/scripts.git/snapshot.sh &>> $CURRENT/log.txt
 
 # run the import
 sudo -u kor $RUBY $ROOT/import_ALL_2020.rb &>> $CURRENT/log.txt
