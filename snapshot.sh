737efc4095e26f51c4aa5018b07d4ce9761cdd4c
Moritz Schepp <moritz.schepp@gmail.com>
2021-08-23 15:57:24 -0700

    make snapshots use borgbackup

--- snapshot.sh
+++ snapshot.sh
@@ -1,20 +1,44 @@
 #!/bin/bash -e
 
+CMD=${1:-manual}
+
+# ensure db dump is up to date
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $ROOT/.env
-
-TS=$(date '+%Y%m%d_%H%M%S')
-TARGET="$BACKUP_DIRECTORY/$TS"
-
-echo "creating backup in $TARGET"
+REPO="$BACKUP_DIRECTORY/borg"
 
 # sudo systemctl stop httpd
 
-mkdir -p $TARGET
-mysqldump -u root -p"$MYSQL_PASSWORD" kor_production | gzip -c > $TARGET/dump.sql.gz
-rsync -a $KOR_SHARED/ $TARGET/shared/
+mysqldump -u root -p"$MYSQL_PASSWORD" kor_production | gzip -c > $KOR_SHARED/dump.sql.gz
 
 # sudo systemctl start httpd
+
+NAME="kor"
+BIN="/usr/bin/borg"
+OPTS="--compression none -v --show-rc --progress --info"
+OPTS="$OPTS --stats --patterns-from $ROOT/snapshot.patterns"
+export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
+
+function init {
+  $BIN init --encryption=repokey $REPO
+}
+
+function list {
+  $BIN list -v $REPO
+}
+
+function nightly {
+  # do the backup and clean up
+  $BIN create $OPTS $REPO::$NAME-{now:%Y-%m-%d-%H-%M-%S}
+  $BIN prune -v --list --keep-daily=30 --keep-weekly=8 --keep-monthly=24 --prefix=$NAME $REPO
+}
+
+function manual {
+  $BIN delete $REPO::$NAME-manual
+  $BIN create $OPTS $REPO::$NAME-manual /
+}
+
+$CMD
 
 echo "done"
