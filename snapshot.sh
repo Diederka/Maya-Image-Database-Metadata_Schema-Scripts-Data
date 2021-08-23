503efb30da60adb0992fd11ecf7caaa35923cb3e
Moritz Schepp <moritz.schepp@gmail.com>
2021-08-23 16:04:50 -0700

    restructured snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -1,18 +1,10 @@
 #!/bin/bash -e
 
 CMD=${1:-manual}
-
-# ensure db dump is up to date
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $ROOT/.env
 REPO="$BACKUP_DIRECTORY/borg"
-
-# sudo systemctl stop httpd
-
-mysqldump -u root -p"$MYSQL_PASSWORD" kor_production | gzip -c > $KOR_SHARED/dump.sql.gz
-
-# sudo systemctl start httpd
 
 NAME="kor"
 BIN="/usr/bin/borg"
@@ -20,8 +12,15 @@ OPTS="--compression none -v --show-rc --progress --info"
 OPTS="$OPTS --stats --patterns-from $ROOT/snapshot.patterns"
 export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
 
+# ensure db dump is up to date
+function ensure_dump {
+  # sudo systemctl stop httpd
+  mysqldump -u root -p"$MYSQL_PASSWORD" kor_production | gzip -c > $KOR_SHARED/dump.sql.gz
+  # sudo systemctl start httpd
+}
+
 function init {
-  $BIN init --encryption=repokey $REPO
+  $BIN init --encryption none $REPO
 }
 
 function list {
@@ -29,16 +28,19 @@ function list {
 }
 
 function nightly {
+  ensure_dump
+
   # do the backup and clean up
   $BIN create $OPTS $REPO::$NAME-{now:%Y-%m-%d-%H-%M-%S}
   $BIN prune -v --list --keep-daily=30 --keep-weekly=8 --keep-monthly=24 --prefix=$NAME $REPO
 }
 
 function manual {
+  ensure_dump
+  
   $BIN delete $REPO::$NAME-manual
   $BIN create $OPTS $REPO::$NAME-manual /
+  echo "done"
 }
 
 $CMD
-
-echo "done"
