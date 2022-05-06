b3cc9f5b19ea9b527ecfd7fd91c642cc6815d361
Moritz Schepp <moritz.schepp@gmail.com>
2022-05-06 16:10:02 -0700

    sets ruby version for combiner script

--- oai_pmh/run.sh
+++ oai_pmh/run.sh
@@ -2,13 +2,13 @@
 
 ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $ROOT
-
+RUBY="/usr/local/rvm/wrappers/ruby-2.5.7/ruby"
 KOR_ROOT="/home/kor/rack/current"
 
 # RUBY_VERSION=2.6.6
 
-ruby client.rb
-ruby combine.rb
+$RUBY client.rb
+$RUBY combine.rb
 
 mkdir $KOR_ROOT/public/oai-pmh-combined
 cp \
