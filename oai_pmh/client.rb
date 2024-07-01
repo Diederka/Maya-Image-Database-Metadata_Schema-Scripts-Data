df7c2a9af2abcb649507ad6dc6dbae5508ab0ca3
Moritz Schepp <schepp@wendig.io>
2024-07-01 15:53:01 -0700

    adds httpclient cert fix

--- oai_pmh/client.rb
+++ oai_pmh/client.rb
@@ -11,6 +11,17 @@ require 'httpclient'
 require 'nokogiri'
 require 'ruby-progressbar'
 
+# see https://github.com/nahi/httpclient/issues/445
+class HTTPClient
+  alias original_initialize initialize
+
+  def initialize(*args, &block)
+    original_initialize(*args, &block)
+    # Force use of the default system CA certs (instead of the 6 year old bundled ones)
+    @session_manager&.ssl_config&.set_default_paths
+  end
+end
+
 module Kor
   class OaiPmhClient
     def initialize(options = {})
